1ba86d0909e32016db73560fb0938475d47e629c
Jozo Mihaljević <jozo.mihaljevic1@gmail.com>
2025-01-23 15:30:17 +0100

    Changes to match login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,44 @@
 import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
 import {FormsModule} from "@angular/forms";
 import {ItemModalComponent} from "./item-modal/item-modal.component";
 import {ItemService} from "./item.service";
+import {AngularFireAuth, AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [CommonModule, RouterOutlet, FormsModule, ItemModalComponent, RouterModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, ItemModalComponent, RouterModule, AngularFireAuthModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    currentYear: number = new Date().getFullYear();
-    itemCount: number = 0;
+  currentYear: number = new Date().getFullYear();
+  itemCount: number = 0;
+  isLoggedIn = false;
 
-    constructor(private itemService: ItemService) {
-    }
+  constructor(private itemService: ItemService,
+              private afAuth: AngularFireAuth,
+              private router: Router) {
+    this.afAuth.authState.subscribe(user => {
+      this.isLoggedIn = !!user;
+    })
+  }
 
-    ngOnInit() {
-        this.itemService.itemCount$.subscribe((count: number) => {
-            this.itemCount = count;
-        });
-    }
+  ngOnInit() {
+    this.itemService.itemCount$.subscribe((count: number) => {
+      this.itemCount = count;
+    });
+  }
 
-    onCartClick() {
-        this.itemService.showCart();
-    }
+  onCartClick() {
+    this.itemService.showCart();
+  }
+
+  logout() {
+    this.afAuth.signOut().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
